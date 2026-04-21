23c1bf89811d3e3fc05ca2a3056f0cd93fd083c7
Eno951024 <kamo8352@gmail.com>
2026-04-21 17:01:21 +0900

    エラー修正

--- src/photos.service.ts
+++ src/photos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 type Photo = {
   id: string;
@@ -9,7 +9,7 @@ type Photo = {
 
 @Injectable()
 export class PhotosService {
-  private supabase: SupabaseClient<any, 'public', 'public', any>;
+  private supabase: any;
 
   constructor() {
     this.supabase = createClient(
